86ee1f086cfb3237b1a1e6f481239544b76b7d93
tarakuds <taradebby@gmail.com>
2022-07-28 23:39:54 +0100

    file upload

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=monoviewDev
+export POSTGRES_PASSWORD=monoviewDev
+export POSTGRES_HOST=monoviewdev.cz4eply21jfx.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=monobucketdev-939995698435
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
